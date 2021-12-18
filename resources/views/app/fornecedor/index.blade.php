7105f4e0c401693f7071a387f502709567b0b52c
devluciano <61517478+devluciano@users.noreply.github.com>
2021-12-18 17:50:20 -0300

    atualização

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,16 @@
+<h3>Fornecedor</h3>
+
+
+
+{{--Fica o comentario que sera descartado pelo navegador--}}
+
+@php
+   /*
+   if() {
+    */
+
+@endphp
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -26,18 +26,21 @@ Route::get('/login', function(){ return 'Login'; })->name('site.login');
 Route::prefix('/app')->group(function(){
     
 Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
-Route::get('/fornecedores', function(){ return 'Fornecedores'; })->name('app.fornecedores');
+Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
 Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
 });
 
 
-Route::get('/rota1', function() {
-        echo 'Rota 1';
-})->name('site.rota1');
+Route::get('/teste/{p1}/{p2}', 'testeController@teste')->name('teste');
 
-Route::get('/rota2', function() {
-        echo 'Rota 2';
-})->name('site.rota2');
+
+
+// rota de contingência
+Route::fallback(function() {
+        echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para a pagina inicial';
+});
+
+
 
 
 
